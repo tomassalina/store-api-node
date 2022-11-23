6f7f696e77d53cc4d6b6296ba17b287880152dfb
Tomás Salina <salinatomass53@gmail.com>
2022-11-22 22:03:39 -0300

    POST method

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const routerApi = require('./routes');
 
 const app = express();
 const port = 4001;
 
-const routerApi = require('./routes');
+// middlewares
+app.use(express.json());
 
+// routes
 app.get('/', (req, res) => {
   res.send('Hola mundo');
 });
--- routes/products.route.js
+++ routes/products.route.js
@@ -35,4 +35,13 @@ router.get('/:id', (req, res) => {
   });
 });
 
+router.post('/', (req, res) => {
+  const body = req.body;
+
+  res.json({
+    message: 'created',
+    data: body,
+  });
+});
+
 module.exports = router;
