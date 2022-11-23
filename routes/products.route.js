4b8b7197ed593160b45655b58dbf9212762b573f
Tomás Salina <salinatomass53@gmail.com>
2022-11-22 22:09:06 -0300

    PATCH and DELETE methods

--- routes/products.route.js
+++ routes/products.route.js
@@ -44,4 +44,24 @@ router.post('/', (req, res) => {
   });
 });
 
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+
+  res.json({
+    message: 'updated',
+    data: body,
+    id,
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  res.json({
+    message: 'deleted',
+    id,
+  });
+});
+
 module.exports = router;
