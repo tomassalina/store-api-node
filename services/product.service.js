3781a00849ac21bd41ea73ab188229094ca7951b
Tomás Salina <salinatomass53@gmail.com>
2023-01-06 23:38:02 -0300

    feat(db): add sequelize and first query

--- services/product.service.js
+++ services/product.service.js
@@ -1,14 +1,12 @@
 const faker = require('faker');
 const boom = require('@hapi/boom');
 
-const pool = require('../libs/postgres.pool');
+const sequelize = require('../libs/sequelize');
 
 class ProductService {
   constructor() {
     this.products = [];
     this.generate();
-    this.pool = pool;
-    this.pool.on('error', (err) => console.error(err));
   }
 
   generate() {
@@ -31,8 +29,8 @@ class ProductService {
 
   async find() {
     const query = 'SELECT * FROM tasks';
-    const rta = await this.pool.query(query);
-    return rta.rows;
+    const [data] = await sequelize.query(query);
+    return data;
   }
 
   async findOne(id) {
