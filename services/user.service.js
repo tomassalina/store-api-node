5b507d328b037912b93f10afeb0c53e504481f84
Tomás Salina <salinatomass53@gmail.com>
2023-01-07 10:37:49 -0300

    feat(services): CRUD with postgres

--- services/user.service.js
+++ services/user.service.js
@@ -1,12 +1,8 @@
-// const boom = require('@hapi/boom');
+const boom = require('@hapi/boom');
 const { models } = require('../libs/sequelize');
 
 class UserService {
   constructor() {}
-
-  async create(data) {
-    return data;
-  }
 
   async find() {
     const rta = await models.User.findAll();
@@ -14,17 +10,27 @@ class UserService {
   }
 
   async findOne(id) {
-    return { id };
+    const user = await models.User.findByPk(id);
+    if (!user) {
+      throw boom.notFound('user not found');
+    }
+    return user;
+  }
+
+  async create(data) {
+    const newUser = await models.User.create(data);
+    return newUser;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const user = await this.findOne(id);
+    const rta = await user.update(changes);
+    return rta;
   }
 
   async delete(id) {
+    const user = await this.findOne(id);
+    await user.destroy();
     return { id };
   }
 }
